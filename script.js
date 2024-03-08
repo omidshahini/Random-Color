c650bca4b94fb63ef36c875f1270bd0cf1dd90f1
omid shahini <moshahini@yahoo.com>
2024-03-08 12:41:12 +0330

    descriptions added

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
+/* in this script im trying to change background and style color 
+with the help of setInterval which used to repeat a specified function
+ at every given time-interval */
+
 setInterval (function () {
+  // Math.floor rounds down a number to its nearest integer
+  // Math.random rounds a number to the nearest integer
   let r = Math.floor(Math.random() * 256)
   let g = Math.floor(Math.random() * 256)
   let b = Math.floor(Math.random() * 256)
@@ -8,9 +14,10 @@ setInterval (function () {
   let RndmColor = 'rgb(' + r + ',' + g + ',' + b + ')'
   let RndmColor1 = 'rgb(' + r1 + ',' + g1 + ',' + b1 + ')'
 
-  console.log(RndmColor)
-
   let bodyElem = document.querySelector('.title') 
   bodyElem.style.color = RndmColor
   document.body.style.backgroundColor = RndmColor1
+  
+  // every 1000 ms is equal to 1 second
+  // so basicaly our function repeates itself every 1 secound
 }, 1000)
